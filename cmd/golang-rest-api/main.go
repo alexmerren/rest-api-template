448c5c6a491c0b342665078dabc6aad330472642
Alex Merren <67416656+alexmerren@users.noreply.github.com>
2022-02-22 16:55:08 +0000

    Add unit tests (#8)

---
+++ cmd/golang-rest-api/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"golang-rest-api/internal/config"
+	"golang-rest-api/internal/datastore"
+	"golang-rest-api/internal/logger"
+	"golang-rest-api/internal/router"
+	"golang-rest-api/internal/server"
+)
+
+func main() {
+	app, err := ProvideApplication()
+	if err != nil {
+		panic(err)
+	}
+	app.Start()
+	defer app.Stop()
+}
+
+type Application struct {
+	logger    *logger.ZapLogger
+	server    *server.Server
+	datastore *datastore.Datastore
+}
+
+func ProvideApplication() (*Application, error) {
+	filesystem := config.ProvideFilesystem()
+	koanf := config.ProvideKoanf()
+	config, err := config.ProvideConfig(koanf, filesystem)
+	if err != nil {
+		return nil, err
+	}
+
+	logger, err := logger.ProvideLogger(config)
+	if err != nil {
+		return nil, err
+	}
+
+	context := context.Background()
+	datastore, err := datastore.ProvideDatastore(context, config)
+	if err != nil {
+		return nil, err
+	}
+
+	router := router.ProvideRouter(context, logger, datastore)
+	server, err := server.ProvideServer(context, config, logger, datastore, router)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Application{
+		logger:    logger,
+		server:    server,
+		datastore: datastore,
+	}, nil
+}
+
+func (a *Application) Start() {
+	a.server.Run()
+}
+
+func (a *Application) Stop() {
+	_ = a.datastore.CloseDB()
+}
