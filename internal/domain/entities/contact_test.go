8594ed37efde2c6db57705bfec107387ca79e6a1
Alex Merren <alexandermerren@gmail.com>
2022-04-24 11:41:42 +0100

    *

--- internal/domain/entities/contact_test.go
+++ internal/domain/entities/contact_test.go
@@ -7,6 +7,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const (
+	test_name     = "name"
+	test_age      = 1
+	test_birthday = "someday"
+	test_address  = "somewhere"
+	test_gender   = "other"
+)
+
+func TestNewContact(t *testing.T) {
+	contact, err := entities.NewContact(
+		test_name,
+		test_age,
+		test_birthday,
+		test_address,
+		test_gender,
+	)
+
+	assert.Equal(t, 36, len(contact.ID))
+	assert.Equal(t, test_name, contact.Name)
+	assert.Equal(t, test_age, contact.Age)
+	assert.Equal(t, test_birthday, contact.Birthday)
+	assert.Equal(t, test_address, contact.Address)
+	assert.Equal(t, test_gender, contact.Gender)
+	assert.Nil(t, err)
+}
+
 func TestContactValidation(t *testing.T) {
 	var testCases = []struct {
 		name           string
